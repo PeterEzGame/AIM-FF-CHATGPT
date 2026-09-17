7b07c46dd712cc0d9f84c3139324e3c982d733bf
PeterEzGame <nguyenhaidang2507hn@gmail.com>
2026-09-17 13:01:36 +0700

    Create build.gradle.kts

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.haridwg"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.haridwg"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+
+            proguardFiles(
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
+                "proguard-rules.pro"
+            )
+        }
+
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+
+    implementation("androidx.compose.ui:ui:1.9.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.1")
+
+    implementation("androidx.compose.material3:material3:1.3.2")
+
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling:1.9.1"
+    )
+}
